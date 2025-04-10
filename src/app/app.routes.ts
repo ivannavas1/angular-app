c83262bb0b844392389f92a6aa449ebf097b60b8
Ivan <ipicazo@indra.es>
2025-04-10 11:29:42 +0200

    dddd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,4 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path:'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
-    {path:'**', redirectTo: 'home'}];
+    {path:'', redirectTo: '/home' ,pathMatch: 'full'}];
